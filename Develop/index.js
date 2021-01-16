6327f2f564c7bf6e0a850222d9e283ec5126ab34
Jivaka <jivakapyane93@gmail.com>
2021-01-16 13:45:47 -0800

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project",
+      },
+      {
+        type: "input",
+        name: "install",
+        message: "How do you this install app?",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "How do you test this app?",
+      },
 ];
 
 // function to write README file
