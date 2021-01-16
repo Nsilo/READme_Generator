5f98ab20f05dbee63792fc711a3cf852a29368a2
Jivaka <jivakapyane93@gmail.com>
2021-01-16 14:22:40 -0800

    added information to markdown code and added questions to index

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 const inquirer = require("inquirer");
 
 // array of questions for user
@@ -7,7 +8,7 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is the name of your project?",
+        message: "What is the title of the project?",
       },
       {
         type: "input",
@@ -16,7 +17,7 @@ const questions = [
       },
       {
         type: "input",
-        name: "install",
+        name: "installation",
         message: "How do you this install app?",
       },
       {
@@ -24,15 +25,53 @@ const questions = [
         name: "test",
         message: "How do you test this app?",
       },
+      {
+        type: "input",
+        message: "How do you use this app?",
+        name: "usage",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Choose a license",
+        choices: ["MIT", "Apache 2.0", "GPL", "BSD", "Mozilla", "ISC", "none"]
+      },
+      {
+        type: "input",
+        name: "contributors",
+        message: "Who are all the contributors?",
+      },
+      {
+        type: "input",
+        name: "GitHub",
+        message: "Enter your github username",
+      },
+      {
+        type: "input",
+        name: "Email",
+        message:  "Please enter your email",
+      },
+      {
+        type: "input",
+        name: "Questions",
+        message: "What is best way to reach you with questions?",
+      },
+   
+   
+    
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(),fileName),data)
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile("GeneratedReadme.md", generateMarkdown({ ...data }));
+    })
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,52 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
 
-`;
-}
+  ## Description
+  ${data.description}
+  ---
+  ## Table of Contents
+  * [Installation](#installation)
 
-module.exports = generateMarkdown;
+  * [Usage](#usage)
+
+  * [Liscense](#license)
+
+  * [Contributors](#contributors)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ---
+
+  ## Installation
+  To install necessary dependencies, run the following command :
+
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+  ${data.usage}
+
+  ## License 
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Test
+  To test application run the following command:
+
+  \`\`\`
+  ${data.test} 
+  \`\`\`
+
+  ## Questions
+  If you have any questions about the repo, please contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+  `;
+  }
+  
+  module.exports = generateMarkdown;
